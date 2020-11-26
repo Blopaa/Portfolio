22187ebd8b24763d0fbc25a12c4be657c3d488d8
Pablo Sabater <pabloosabaterr@gmail.com>
2020-11-26 12:24:26 +0100

    middleview

---
+++ src/components/middleview/MiddleView.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MiddleView = () => {
+  return (
+    <div className="middleview__container">
+      <h2 id="abajo" className="subtitle middleview__title">
+        My Projects
+      </h2>
+    </div>
+  );
+};
+
+export default MiddleView;
